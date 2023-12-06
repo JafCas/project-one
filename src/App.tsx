d6c35523a1940fab2f87bb1a395a01f80adf28bf
Jafet Castillo <jafetkaid@gmail.com>
2023-12-06 00:25:53 -0600

    Set initial components for adding entries

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+type Entry = {
+  id: number;
+  title: string;
+  content: string;
+};
 
 function App() {
+  const [entries, setEntries] = useState<Entry[]>([
+    {
+      id: 1,
+      title: "note title",
+      content: "content 1",
+    },
+    {
+      id: 2,
+      title: "note title",
+      content: "content 2",
+    },
+    {
+      id: 3,
+      title: "note title",
+      content: "content 3",
+    },
+  ]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const newEntry: Entry = {
+      id: entries.length + 1,
+      title: title,
+      content: content,
+    };
+    setEntries([newEntry, ...entries]);
+    setTitle("");
+    setContent("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form" onSubmit={(event) => handleSubmit(event)}>
+        <input
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+          placeholder="title"
+          required
+        ></input>
+        <textarea
+          value={content}
+          onChange={(event) => {
+            setContent(event.target.value);
+          }}
+          placeholder="content"
+          rows={10}
+          required
+        ></textarea>
+        <button type="submit">Add Entry</button>
+      </form>
+      <div className="notes-grid">
+        {entries.map((entry) => (
+          <div className="note-item" key={entry.id}>
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{entry.title}</h2>
+            <p>{entry.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
